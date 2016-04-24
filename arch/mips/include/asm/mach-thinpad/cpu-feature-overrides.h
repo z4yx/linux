decf7e9dffd3bce2bedbe2c63c57eb08ad89eb09
zhang <zz593141477@gmail.com>
2016-04-24 14:07:03 +0800

    remove some cpu features dependency

---
+++ arch/mips/include/asm/mach-thinpad/cpu-feature-overrides.h
@@ -0,0 +1,10 @@
+#ifndef __ASM_MACH_THINPAD_CPU_FEATURE_OVERIDES_
+#define __ASM_MACH_THINPAD_CPU_FEATURE_OVERIDES_ 
+
+#define cpu_has_watch         0
+#define cpu_has_llsc          0
+#define cpu_has_divec         0
+#define cpu_has_mcheck        0
+#define cpu_has_ejtag         0
+
+#endif
