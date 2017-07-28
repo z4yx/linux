055a3e0177cbf65936432f1eaa947275d73153c2
ZhangYuxiang <zz593141477@gmail.com>
2017-07-29 01:40:26 +0800

    fix arch irq init function

--- arch/mips/thinpad/intc.c
+++ arch/mips/thinpad/intc.c
@@ -1,8 +1,8 @@
 /*
- * Xilfpga interrupt controller setup
+ * NaiveMIPS interrupt controller setup
  *
- * Copyright (C) 2015 Imagination Technologies
- * Author: Zubair Lutfullah Kakakhel <Zubair.Kakakhel@imgtec.com>
+ * Copyright (C) 2017 Tsinghua Univ.
+ * Author: Yuxiang Zhang
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -11,15 +11,17 @@
 
 #include <linux/of.h>
 #include <linux/of_irq.h>
+#include <linux/irq.h>
+#include <linux/irqchip.h>
 
 #include <asm/irq_cpu.h>
 
-static struct of_device_id of_irq_ids[] __initdata = {
-	{ .compatible = "mti,cpu-interrupt-controller", .data = mips_cpu_irq_of_init },
-	{},
-};
-
 void __init arch_init_irq(void)
 {
-	of_irq_init(of_irq_ids);
+    pr_devel("arch_init_irq\n");
+	// of_irq_init(of_irq_ids);
+    irqchip_init();
 }
+
+IRQCHIP_DECLARE(mips_cpu_intc, "mti,cpu-interrupt-controller",
+         mips_cpu_irq_of_init);
